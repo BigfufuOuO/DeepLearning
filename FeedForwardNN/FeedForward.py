841fca3d6571010be2ae15137d463c9ad437d3ab
BigfufuOuO <923525301@qq.com>
2024-03-25 21:58:08 +0800

    add

--- FeedForwardNN/FeedForward.py
+++ FeedForwardNN/FeedForward.py
@@ -27,7 +27,7 @@ class Feed_Forward_Network(nn.Module):
 def plot_fig(x_val, y_val, y_pred, output_file):
     plt.figure(figsize=(8, 6))
     plt.scatter(x_val, y_pred, label='Predited Points', color='blue', alpha=0.5)
-    plt.scatter(x_val, y_val, label='Original Points', color='red', alpha=0.5)
+    # plt.scatter(x_val, y_val, label='Original Points', color='red', alpha=0.5)
     x = np.linspace(1, 16, 100)
     y = np.log2(x) + np.cos(np.pi * x / 2)
     plt.plot(x, y, label='True cure', color='black', alpha=0.3)
