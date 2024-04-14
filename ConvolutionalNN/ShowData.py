64d75e344d7244ffba03d110cb89f172ed6374b7
BigfufuOuO <923525301@qq.com>
2024-04-14 22:20:06 +0800

    add data loader

---
+++ ConvolutionalNN/ShowData.py
@@ -0,0 +1,30 @@
+from DataLoad import load_data
+import matplotlib.pyplot as plt
+import numpy as np
+
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
+          'dog', 'frog', 'horse', 'ship', 'truck']
+
+# Define the dimensions of the plot grid 
+W_grid = 5
+L_grid = 5
+
+train_loader, val_loader, test_loader = load_data()
+def plot_images(set=train_loader):
+    fig, axes = plt.subplots(L_grid, W_grid, figsize = (17,17))
+    axes = axes.ravel() # flatten
+
+    images, label = next(iter(set)) # get some samples from the train data
+
+    for i in np.arange(0, W_grid * L_grid): 
+        image = images[i]
+        image = image / 2 + 0.5
+        npimg = image.numpy()
+        axes[i].imshow(np.transpose(npimg, (1, 2, 0)))
+        axes[i].set_title(labels[label[i].item()])
+        axes[i].axis('off')
+        
+    plt.subplots_adjust(hspace=0.4)
+    plt.show()
+
+plot_images(val_loader)
---
+++ ConvolutionalNN/report.md
@@ -0,0 +1,3 @@
+https://www.kaggle.com/code/faressayah/cifar-10-images-classification-using-cnns-88
+https://www.kaggle.com/code/shadabhussain/cifar-10-cnn-using-pytorch
+https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
