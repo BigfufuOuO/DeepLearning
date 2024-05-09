dda0e21aa66337bb4fd1ae3eeb17936e0ed99c5a
BigfufuOuO <923525301@qq.com>
2024-05-09 17:39:18 +0800

    modify files

--- GraphConvolution/DataLoad.py
+++ GraphConvolution/DataLoad.py
@@ -1,7 +1,45 @@
 # Get data from dataset
-import panda as pd
+import os
+import pandas as pd
 import numpy as np
 
-Cora_raw_data_content = pd.read_csv('data/cora/cora.content', sep='\t', header=None)
-Cora_raw_data_site = pd.read_csv('data/cora/cora.cites', sep='\t', header=None)
+class DataProcessor:
+    def __init__(self) -> None:
+        pass
+
+    def load_data(self, dataset):
+        current_path = os.path.abspath(os.path.dirname(__file__))
+        data_path = os.path.join(current_path, 'data/')
+        data_path = os.path.join(data_path, dataset + '/' + dataset)
+
+        raw_data_content = pd.read_csv(data_path + '.content', sep='\t', header=None)
+        raw_data_cite = pd.read_csv(data_path + '.cites', sep='\t', header=None)
+
+        # extract feature and label
+        features = raw_data_content.iloc[:, 1:-1].values
+        labels = raw_data_content.iloc[:, -1].values
+
+        # construct adjacency matrix
+        ## Construct zero matrix
+        print(raw_data_content.shape)
+        num_codes = raw_data_content.shape[0] # 2708
+        Matrix_adjacency = np.zeros((num_codes, num_codes))
+
+        ## transform index
+        index_raw = list(raw_data_content.iloc[:, 0])
+        index_new = list(range(num_codes))
+        Map_index = dict(zip(index_raw, index_new))
+
+        ## construct adjacency matrix
+        for i in range(raw_data_cite.shape[0]):
+            index1 = Map_index[raw_data_cite.iloc[i, 0]]
+            index2 = Map_index[raw_data_cite.iloc[i, 1]]
+            Matrix_adjacency[index1, index2] = 1
+            Matrix_adjacency[index2, index1] = 1
+
+        # construct degree matrix
+        Matrix_degree = np.diag(np.sum(Matrix_adjacency, axis=1))
+        
+cora = DataProcessor()
+cora.load_data('cora')
 
