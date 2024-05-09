f94505e9f5524d0574e8dd9c2d658a21c0459768
BigfufuOuO <923525301@qq.com>
2024-05-09 18:03:18 +0800

    modify

--- GraphConvolution/DataLoad.py
+++ GraphConvolution/DataLoad.py
@@ -4,13 +4,13 @@ import pandas as pd
 import numpy as np
 
 class DataProcessor:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, dataset='cora'):
+        self.dataset = dataset
 
-    def load_data(self, dataset):
+    def load_data_adjacency(self):
         current_path = os.path.abspath(os.path.dirname(__file__))
         data_path = os.path.join(current_path, 'data/')
-        data_path = os.path.join(data_path, dataset + '/' + dataset)
+        data_path = os.path.join(data_path, self.dataset + '/' + self.dataset)
 
         raw_data_content = pd.read_csv(data_path + '.content', sep='\t', header=None)
         raw_data_cite = pd.read_csv(data_path + '.cites', sep='\t', header=None)
@@ -38,7 +38,29 @@ class DataProcessor:
             Matrix_adjacency[index2, index1] = 1
 
         # construct degree matrix
-        Matrix_degree = np.diag(np.sum(Matrix_adjacency, axis=1))
+        # Matrix_degree = np.diag(np.sum(Matrix_adjacency, axis=1))
+        
+        
+    def load_data_sparse(self):
+        current_path = os.path.abspath(os.path.dirname(__file__))
+        data_path = os.path.join(current_path, 'data/')
+        data_path = os.path.join(data_path, self.dataset + '/' + self.dataset)
+
+        raw_data_content = pd.read_csv(data_path + '.content', sep='\t', header=None)
+        raw_data_cite = pd.read_csv(data_path + '.cites', sep='\t', header=None)
+
+        # extract feature and label
+        features = raw_data_content.iloc[:, 1:-1].values
+        labels = raw_data_content.iloc[:, -1].values
+        
+        # Construct sparse matrix
+        ## transform index
+        num_codes = raw_data_content.shape[0] # 2708
+        index_raw = list(raw_data_content.iloc[:, 0])
+        index_new = list(range(num_codes))
+        Map_index = dict(zip(index_raw, index_new))
+        
+        ## construct sparse matrix
         
 cora = DataProcessor()
 cora.load_data('cora')
